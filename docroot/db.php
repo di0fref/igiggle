c68c510fcff7542afef24cf0ce0dcd2897419670
Fredrik Fahlstad <fredrik@fahlstad.se>
2013-10-20 01:43:14 +0200

    removed some stuff

--- docroot/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$user_id = "1";
-
-// DB connect parameters
-$server = "localhost";
-$user = "root";
-$password = "root";
-$database = "s6411_igiggle";
--- docroot/scripts/script.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `iNettuts` (
-`user_id` VARCHAR( 50 ) NOT NULL ,
-`config` TEXT NOT NULL ,
-PRIMARY KEY ( `user_id` )
-);
